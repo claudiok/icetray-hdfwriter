27a04f7094f1c63f88d6ffe75d7c6ec49773f634
Jakob van Santen <jvansanten@gmail.com>
2010-02-18 23:02:07 +0000

    Don't pull in PyTables support on Python < 2.4.
    git-svn-id: http://code.icecube.wisc.edu/svn/projects/hdf-writer/branches/new-internals-2@61741 16731396-06f5-0310-8873-f7f720988828

--- python/__init__.py
+++ python/__init__.py
@@ -1,4 +1,10 @@
 from icecube.load_pybindings import load_pybindings
 load_pybindings(__name__, __path__)
 
-import pytables_ext
+import sys
+major,minor = sys.version_info[:2]
+# pytables only runs on 2.4 anyhow, and this script uses decorators
+if (major >= 2 and minor >= 4):
+    import pytables_ext
+    
+del sys,major,minor
