98331cff8ab5db74be5ac4d654c7ca112f16c0dc
Nathan Whitehorn <nwhitehorn@icecube.wisc.edu>
2011-09-05 06:32:35 +0000

    A simple example of a tray segment to synthesize a one-command HDF writer.
    git-svn-id: http://code.icecube.wisc.edu/svn/projects/hdfwriter/trunk@79593 16731396-06f5-0310-8873-f7f720988828

--- python/__init__.py
+++ python/__init__.py
@@ -1,5 +1,5 @@
 from icecube.load_pybindings import load_pybindings
-import icecube.tableio # pull in direct dependencies
+from icecube import icetray, tableio
 
 import sys
 major,minor = sys.version_info[:2]
@@ -8,8 +8,23 @@ if (major >= 2 and minor >= 4):
     import pytables_ext
 
 # clean the local dictionary
-del sys,major,minor,icecube
+del sys,major,minor
 
 load_pybindings(__name__, __path__)
 
+@icetray.traysegment
+def I3HDFWriter(tray, name, Output=None, **kwargs):
+	"""Tabulate data to an HDF5 file.
 
+	Arguments:
+		Output: Path to output file
+		Keys: Key names of frame objects to tabulate
+		Streams: Subevent streams to include
+		(See I3TableWriter documentation for others)
+	"""
+	tabler = I3HDFTableService(Output)
+	tray.AddModule(tableio.I3TableWriter, name, TableService=tabler,
+	    **kwargs)
+
+# clean the local dictionary
+del icetray,tableio
