97f47f9b4c83c35d5557aa781661a201bfe20a21
Alex Olivas <olivas@icecube.umd.edu>
2012-05-04 17:19:19 +0000

    reverting back to loading projects manually.
    git-svn-id: http://code.icecube.wisc.edu/svn/projects/hdfwriter/trunk@87951 16731396-06f5-0310-8873-f7f720988828

--- private/pybindings/module.cxx
+++ private/pybindings/module.cxx
@@ -11,6 +11,7 @@
  * @author Jakob van Santen <vansanten@wisc.edu> Last changed by: $LastChangedBy$
  */
 
+#include <icetray/load_project.h>
 
 namespace bp = boost::python;
 
@@ -23,6 +24,8 @@ BOOST_PP_SEQ_FOR_EACH(I3_REGISTRATION_FN_DECL, ~, REGISTER_THESE_THINGS)
 
 I3_PYTHON_MODULE(hdfwriter)
 {
+
+  load_project("hdfwriter", false); 
 
   BOOST_PP_SEQ_FOR_EACH(I3_REGISTER, ~, REGISTER_THESE_THINGS);
 }
