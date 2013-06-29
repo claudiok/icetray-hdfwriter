b7c71f23148f45b221c5385e36969220683fd3f5
Nathan Whitehorn <nwhitehorn@icecube.wisc.edu>
2013-06-29 19:54:02 +0000

    You can't throw strings as exceptions anymore, so turn one into a RuntimeError. The rest just rethrow a perfectly good ImportError as a string, so keep the original RuntimeError. Also some s/long/int that does not change what this does on Python >= 2.3.
    git-svn-id: http://code.icecube.wisc.edu/svn/projects/hdfwriter/trunk@107679 16731396-06f5-0310-8873-f7f720988828

--- resources/scripts/demo.py
+++ resources/scripts/demo.py
@@ -31,16 +31,10 @@ from icecube import icetray,dataclasses,dataio,tableio
 from I3Tray import I3Tray
 
 if options.format == 'hdf5':
-	try:
-		from icecube import hdfwriter
-	except ImportError:
-		raise "Couldn't find the HDF writer service"
+	from icecube import hdfwriter
 	tabler = hdfwriter.I3HDFTableService(outfile,options.compression)
 elif options.format == 'root':
-	try:
-		from icecube import rootwriter
-	except ImportError:
-		raise "Couldn't find the ROOT writer service"
+	from icecube import rootwriter
 	tabler = rootwriter.I3ROOTTableService(outfile,options.compression)
 elif options.format == 'csv':
 	tabler = tableio.I3CSVTableService(outfile[:-4] + '_csv')
