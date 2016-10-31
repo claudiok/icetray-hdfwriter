d7e5f0b2e3f65d950c1d1efe73d76d5e339c50f8
Jakob van Santen <jvansanten@gmail.com>
2016-10-31 07:44:38 +0000

    pytables_ext conflicts with PyTables 3.3. Fix it permanently.
    git-svn-id: http://code.icecube.wisc.edu/svn/projects/hdfwriter/trunk@151233 16731396-06f5-0310-8873-f7f720988828

--- python/__init__.py
+++ python/__init__.py
@@ -1,15 +1,6 @@
 from icecube.load_pybindings import load_pybindings
 from icecube import icetray, tableio, dataio
 
-import sys
-major,minor = sys.version_info[:2]
-# pytables only runs on 2.4 anyhow, and this script uses decorators
-if (major, minor) >= (2, 4):
-    from icecube.hdfwriter import pytables_ext
-
-# clean the local dictionary
-del sys,major,minor
-
 load_pybindings(__name__, __path__)
 
 @icetray.traysegment_inherit(tableio.I3TableWriter,
